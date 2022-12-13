96b9ff2e7a467faea186b0ce11558e91abe5ca9b
William <wonko7@gmail.com>
2022-12-13 19:01:12 +0100

    [emacs] exwm

--- emacs.d/exwm.el
+++ emacs.d/exwm.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (C) 2022 William
 ;;
-;; Author: William <https://github.com/wjc>
+;; Author: William <https://github.com/wonko7>
 ;; Maintainer: William <john@doe.com>
 ;; Created: November 23, 2022
 ;; Modified: November 23, 2022
@@ -21,9 +21,61 @@
 
 (require 'exwm)
 (require 'exwm-config)
-(exwm-config-default)
+;; (exwm-config-default)
 
-(load "~/.emacs.d/init.el") ;; I guess?
+
+(setq exwm-workspace-number 9)
+(setq exwm-input-prefix-keys
+      '(?\C-x
+        ?\C-u
+        ?\C-:
+        ?\C-h
+        ?\M-:
+        ;?\C-?RET   ; C-RET would be bad ass
+        ?\C-\   ; I want whitespace here
+        ))
+
+;; (evil-define-key '(normal input) exwm-mode-map
+;;   "ESC" 'exwm-input-send-next-key
+;;   "C-q" 'exwm-input-send-next-key
+;;   )
+
+;; Ctrl+Q will enable the next key to be sent directly
+(define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+;; Set up global key bindings.  These always work, no matter the input state!
+;; Keep in mind that changing this list after EXWM initializes has no effect.
+(setq exwm-input-global-keys
+      `(
+        ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+        ([?\s-r] . exwm-reset)
+
+        ;; Move between windows
+        ([?\s-h] . windmove-left)
+        ([?\s-l] . windmove-right)
+        ([?\s-k] . windmove-up)
+        ([?\s-j] . windmove-down)
+
+        ;; Launch applications via shell command
+        ([?\s-&] . (lambda (command)
+                     (interactive (list (read-shell-command "$ ")))
+                     (start-process-shell-command command nil command)))
+
+        ;; Switch workspace
+        ([?\s-w] . exwm-workspace-switch)
+        ([?\s-`] . (lambda () (interactive) (exwm-workspace-switch-create 0)))
+
+        ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+        ,@(mapcar (lambda (i)
+                    `(,(kbd (format "s-%d" i)) .
+                      (lambda ()
+                        (interactive)
+                        (exwm-workspace-switch-create ,i))))
+                  (number-sequence 0 9))))
+
+(exwm-enable)
+
+(load "~/.emacs.d/init.el")
 
 (provide 'exwm)
 ;;; exwm.el ends here
