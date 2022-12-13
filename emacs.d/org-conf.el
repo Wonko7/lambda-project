3dd60610d403fa4aa5a12db96217bdfd6c75ed51
William <wonko7@gmail.com>
2022-12-13 20:47:38 +0100

    [emacs] fix org warning

--- emacs.d/org-conf.el
+++ emacs.d/org-conf.el
@@ -206,7 +206,7 @@
 (defun transform-square-brackets-to-round-ones (string-to-transform)
   "Transforms [ into ( and ] into ), other chars left unchanged."
   (concat
-   (mapcar #'(lambda (c) (if (equal c ?[) ?\( (if (equal c ?]) ?\) c))) string-to-transform)))
+   (mapcar #'(lambda (c) (if (equal c ?\[) ?\( (if (equal c ?\]) ?\) c))) string-to-transform)))
 
 (defvar my/daily-header "#+title: %<%Y-%m-%d>\n#+category: %<%Y-%m-%d>")
 (defvar my/daily-file "%<%Y-%m-%d>.org")
