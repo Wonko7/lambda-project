71e699ceef12d2b7c24a6882686e4a71fc4a575f
William <wonko7@gmail.com>
2022-12-22 14:09:21 +0100

    [emacs] stop littering fs with file backups

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,6 +23,17 @@
 
 (setq even-window-sizes nil)     ; avoid resizing
 
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
+;; (setq tramp-backup-directory-alist backup-directory-alist)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; spell
 
