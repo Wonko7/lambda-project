6d6bc8493e577c26104129e5ebf245c78b37b8a6
William <wonko7@gmail.com>
2023-01-04 15:01:23 +0100

    [emacs][ement] don't auto start

--- emacs.d/communication.el
+++ emacs.d/communication.el
@@ -1,6 +1,8 @@
 (require 'ement)
 
 (setq ement-room-list-avatars nil)
-(ement-connect :user-id "@wonko7:matrix.org" :password (auth-source-pass-get 'secret "web/matrix/wonko7") :uri-prefix "http://127.0.0.1:8666")
+
+(defun my/ement-init ()
+  (ement-connect :user-id "@wonko7:matrix.org" :password (auth-source-pass-get 'secret "web/matrix/wonko7") :uri-prefix "http://127.0.0.1:8666"))
 
 (provide 'conf/communication)
