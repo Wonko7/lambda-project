69fec2b6ecd843329f2aa5e5c7160b9167f57c15
William <wonko7@gmail.com>
2023-01-05 18:02:17 +0100

    [emacs] ement is up and running

--- emacs.d/communication.el
+++ emacs.d/communication.el
@@ -1,8 +1,10 @@
 (require 'ement)
 
-(setq ement-room-list-avatars nil)
+(setq ement-room-list-avatars nil) ;; FIXME/review bug workaround
+(setq ement-room-message-format-spec "%S> %W%B%r%R%t")
 
 (defun my/ement-init ()
+  (interactive)
   (ement-connect :user-id "@wonko7:matrix.org" :password (auth-source-pass-get 'secret "web/matrix/wonko7") :uri-prefix "http://127.0.0.1:8666"))
 
 (provide 'conf/communication)
