39281c115b491b32596020d9cde3d6ecf7ae53a7
William <wonko7@gmail.com>
2023-01-09 15:00:08 +0100

    [emacs] web tools paste link: input after cursor

--- emacs.d/org-conf.el
+++ emacs.d/org-conf.el
@@ -154,6 +154,19 @@ space rather than before."
       (evil-append 0)
       ad-do-it
       (evil-normal-state))))
+;; FIXME merge with ^
+(defadvice org-web-tools-insert-link-for-url (around append-if-in-evil-normal-mode activate compile)
+  "If in evil normal mode and cursor is on a whitespace character, then go into
+append mode first before inserting the link. This is to put the link after the
+space rather than before."
+  (let ((is-in-evil-normal-mode (and (bound-and-true-p evil-mode)
+                                     (not (bound-and-true-p evil-insert-state-minor-mode))
+                                     (looking-at "[[:blank:]]"))))
+    (if (not is-in-evil-normal-mode)
+        ad-do-it
+      (evil-append 0)
+      ad-do-it
+      (evil-normal-state))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; super agenda
