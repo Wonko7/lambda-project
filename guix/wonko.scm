3defd4c4ca013aecdc1edf0d7e7c274af2501a68
William <wonko7@gmail.com>
2023-04-11 21:52:10 +0200

    [guix][home] style points

--- guix/wonko.scm
+++ guix/wonko.scm
@@ -77,16 +77,18 @@
  (pkgs)
  (stateful-prelude))
 
-(display (spock-say (string-append "building HOME for " (ship-name %ship)))
-         (current-error-port))
+(display
+ (spock-say (string-append "building HOME for " (ship-name %ship)))
+ (current-error-port))
 (display "\n"
          (current-error-port))
 
 (define %emacs-values
-  #~(progn (setq my/font #$%font
-                 my/font-size #$(ship-emacs-font-size %ship)
-                 my/modeline-height #$(ship-emacs-modeline-height %ship))
-           (provide 'conf/generated-values)))
+  #~(progn
+     (setq my/font #$%font
+           my/font-size #$(ship-emacs-font-size %ship)
+           my/modeline-height #$(ship-emacs-modeline-height %ship))
+     (provide 'conf/generated-values)))
 
 (define %aliases
   `(("g" . "git")
@@ -124,8 +126,7 @@
     ibhagwan-picom
     synergy
     dunst
-
-    ;; basic system stuff
+    ;; yes also man pages plz
     man-db)))
 
  (services
@@ -148,8 +149,10 @@
                 ("GUIX_EXTRA_PROFILES" . "$HOME/.guix-extra-profiles")
                 ("PASSWORD_STORE_DIR" . "/data/pass")
                 ("RIPGREP_CONFIG_PATH" . "$HOME/.config/ripgrep/ripgreprc")
-                ("GDK_SCALE" . ,(number->string (ship-gdk-scale %ship)))
-                ("GDK_DPI_SCALE" . ,(number->string (ship-gdk-dpi-scale %ship)))
+                ("GDK_SCALE" . ,(number->string
+                                 (ship-gdk-scale %ship)))
+                ("GDK_DPI_SCALE" . ,(number->string
+                                     (ship-gdk-dpi-scale %ship)))
                 ("QT_QPA_PLATFORM_PLUGIN_PATH" . "$HOME/.guix-home/profile/lib/qt5/plugins")
                 ("QT_STYLE_OVERRIDE" . "kvantum")
                 ("XDG_CURRENT_DESKTOP" . "qt5ct")))
@@ -171,8 +174,7 @@ test -r ~/.opam/opam-init/init.sh && . ~/.opam/opam-init/init.sh > /dev/null 2> 
    (simple-service 'emacsd-config-files
                    home-files-service-type
                    (map
-                    (lambda
-                        (file)
+                    (lambda (file)
                       `(,(string-append ".emacs.d/" file)
                         ,(local-file
                           (string-append conf-root-dir "/emacs.d/" file))))
@@ -193,8 +195,7 @@ test -r ~/.opam/opam-init/init.sh && . ~/.opam/opam-init/init.sh > /dev/null 2> 
    (simple-service 'emacsd-snippets-config-files
                    home-files-service-type
                    (map
-                    (lambda
-                        (file)
+                    (lambda (file)
                       `(,(string-append ".emacs.d/snippets/" file)
                         ,(local-file
                           (string-append conf-root-dir "/emacs.d/snippets/" file))))
@@ -204,20 +205,23 @@ test -r ~/.opam/opam-init/init.sh && . ~/.opam/opam-init/init.sh > /dev/null 2> 
 
    (simple-service 'emacsd-generated-config-files
                    home-files-service-type
-                   (list `(".emacs.d/generated-values.el"
-                           ,(scheme-file "_" %emacs-values))))
+                   (list
+                    `(".emacs.d/generated-values.el"
+                      ,(scheme-file "_" %emacs-values))))
 
    (simple-service
     'config-files
     home-files-service-type
     ;; exwm config is outside of .emacs.d:
     `((".exwm"
-       ,(local-file (string-append conf-root-dir  "/emacs.d/exwm.el")))
+       ,(local-file
+         (string-append conf-root-dir  "/emacs.d/exwm.el")))
       (".emacs.d/aliases"
        ,(plain-file "aliases"
                     (emacs-eshell-aliases-configuration %aliases)))
       (".x-config"
-       ,(program-file "x-config" (ship-x-config %ship)))
+       ,(program-file "x-config"
+                      (ship-x-config %ship)))
       (".xsession"
        ,(program-file
          "xsession"
@@ -230,71 +234,102 @@ test -r ~/.opam/opam-init/init.sh && . ~/.opam/opam-init/init.sh > /dev/null 2> 
             (xsetroot . "-cursor_name left_ptr")
             (setxkbmap . "dvorak")
             (xmodmap . "~/.config/x-config/common.xmodmap")
-            (xmodmap . ,(string-append ".config/x-config/" (ship-name %ship) ".xmodmap"))
-            (feh . ,(string-append "--bg-scale '" (ship-wallpaper %ship) "'"))
+            (xmodmap . ,(string-append ".config/x-config/"
+                                       (ship-name %ship)
+                                       ".xmodmap"))
+            (feh . ,(string-append "--bg-scale '"
+                                   (ship-wallpaper %ship)
+                                   "'"))
             ("~/.x-config"  . "")
             (,#~(string-append  "exec " #$dbus "/bin/dbus-launch --exit-with-session")
                 . #$(file-append emacs-exwm "/bin/exwm"))))))
       ("spock"
        ,(program-file
          "spock"
-         (with-imported-modules '((spock))
-           #~(begin
-               (use-modules (spock))
-               (display (spock-say "live long & prosper!"))
-               (newline)))))
+         (with-imported-modules
+          '((spock))
+          #~(begin
+              (use-modules
+               (spock))
+              (display
+               (spock-say "live long & prosper!"))
+              (newline)))))
       ;; utils:
       (".config/git/config"
-       ,(local-file (string-append conf-root-dir "/misc/gitconfig")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/gitconfig")))
       (".config/git/attributes"
-       ,(local-file (string-append conf-root-dir "/misc/gitattributes")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/gitattributes")))
       (".config/ripgrep/ripgreprc"
-       ,(local-file (string-append conf-root-dir "/misc/ripgreprc")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/ripgreprc")))
       ;; GTK & QT configs:
       (".gtkrc-2.0"
-       ,(local-file (string-append conf-root-dir "/misc/gtkrc-2.0")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/gtkrc-2.0")))
       (".config/gtk-3.0/settings.ini"
-       ,(local-file (string-append conf-root-dir "/misc/gtkrc-3.0")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/gtkrc-3.0")))
       (".config/Kvantum/kvantum.kvconfig"
-       ,(local-file (string-append conf-root-dir "/misc/kvantum.kvconfig")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/kvantum.kvconfig")))
       (".config/Kvantum/KvGnomish#/KvGnomish#.kvconfig"
-       ,(local-file (string-append conf-root-dir "/misc/kv_yggdrasill.kvconfig")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/kv_yggdrasill.kvconfig")))
       (".config/qt5ct/qt5ct.conf"
-       ,(local-file (string-append conf-root-dir "/misc/qt5ct.conf")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/qt5ct.conf")))
       ;; my X stuff:
       (".XCompose"
-       ,(local-file (string-append conf-root-dir "/misc/XCompose")))
-      (,(string-append ".config/x-config/" (ship-name %ship) ".xmodmap")
-       ,(local-file (string-append conf-root-dir "/misc/" (ship-name %ship) ".xmodmap")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/XCompose")))
+      (,(string-append ".config/x-config/"
+                       (ship-name %ship)
+                       ".xmodmap")
+       ,(local-file
+         (string-append conf-root-dir "/misc/"
+                        (ship-name %ship)
+                        ".xmodmap")))
       (".config/x-config/common.xmodmap"
-       ,(local-file (string-append conf-root-dir "/misc/common.xmodmap")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/common.xmodmap")))
       (".config/picom.conf"
-       ,(plain-file "picom.conf" (picom-configuration (ship-picom-radius %ship))))
+       ,(plain-file "picom.conf"
+                    (picom-configuration
+                     (ship-picom-radius %ship))))
       (".config/dunst/dunstrc"
-       ,(plain-file "dunstrc" (dunst-configuration (ship-font %ship)
-                                                   (ship-dunst-font-size %ship)
-                                                   (ship-dunst-width %ship))))
+       ,(plain-file "dunstrc"
+                    (dunst-configuration
+                     (ship-font %ship)
+                     (ship-dunst-font-size %ship)
+                     (ship-dunst-width %ship))))
       (".config/pantalaimon/pantalaimon.conf"
-       ,(local-file (string-append conf-root-dir "/misc/pantalaimon.conf")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/pantalaimon.conf")))
       (".config/Synergy/Synergy.conf"
-       ,(local-file (string-append conf-root-dir "/misc/Synergy.conf")))
+       ,(local-file
+         (string-append conf-root-dir "/misc/Synergy.conf")))
       ;; deploy secrets
       ("local/bin/secrets"
        ,(program-file
          "secrets"
-         (with-imported-modules '((spock)
-                                  (guix build utils))
-           #~(begin
-               (use-modules (spock)
-                            (guix build utils))
-               (display (spock-say (string-append "deploying SECRETS for " #$(ship-name %ship)))
-                        (current-error-port))
-               (newline (current-error-port))
-               (let ((pass   #$(file-append password-store "/bin/pass"))
-                     (base64 #$(file-append coreutils "/bin/base64"))
-                     (tar    #$(file-append tar "/bin/tar")))
-                 (system (string-append pass " show fleet/" #$(ship-name %ship) "/ssh | "
-                                        base64 " -d | " tar " xz ")))))))))
+         (with-imported-modules
+          '((spock)
+            (guix build utils))
+          #~(begin
+              (use-modules (spock)
+                           (guix build utils))
+              (display (spock-say
+                        (string-append "deploying SECRETS for " #$(ship-name %ship)))
+                       (current-error-port))
+              (newline (current-error-port))
+              (let ((pass   #$(file-append password-store "/bin/pass"))
+                    (base64 #$(file-append coreutils "/bin/base64"))
+                    (tar    #$(file-append tar "/bin/tar")))
+                (system
+                 (string-append pass " show fleet/" #$(ship-name %ship) "/ssh | "
+                                base64 " -d | " tar " xz ")))))))))
 
    (simple-service 'guix-config-files
                    home-files-service-type
@@ -334,8 +369,9 @@ test -r ~/.opam/opam-init/init.sh && . ~/.opam/opam-init/init.sh > /dev/null 2> 
                (shepherd-service
                 (provision '(guix-repl))
                 (start #~(make-forkexec-constructor
-                          (list (string-append #$%home "/.config/guix/current/bin/guix")
-                                "repl" "--listen=tcp:37146")
+                          (list
+                           (string-append #$%home "/.config/guix/current/bin/guix")
+                           "repl" "--listen=tcp:37146")
                           #:environment-variables '("INSIDE_EMACS=1")
                           #:log-file "log/guix-repl.log"))
                 (stop #~(make-kill-destructor))
@@ -343,7 +379,8 @@ test -r ~/.opam/opam-init/init.sh && . ~/.opam/opam-init/init.sh > /dev/null 2> 
                (shepherd-service
                 (provision '(xss-lock))
                 (start #~(make-forkexec-constructor
-                          (list #$(file-append xss-lock "/bin/xss-lock") "--"
+                          (list #$(file-append xss-lock "/bin/xss-lock")
+                                "--"
                                 "/run/setuid-programs/xlock" "-mode" "daisy" "-lockdelay" "10")
                           #:log-file "log/xss-lock.log"))
                 (stop #~(make-kill-destructor))
