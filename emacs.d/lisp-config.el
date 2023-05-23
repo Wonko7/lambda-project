fe8e59a276bf3a960d911104042c2aecba07432c
William <wonko7@gmail.com>
2023-05-23 17:32:44 +0200

    [emacs] configure cleverparens

--- emacs.d/lisp-config.el
+++ emacs.d/lisp-config.el
@@ -3,6 +3,12 @@
 
 (require 'eval-sexp-fu)
 (require 'evil-cleverparens)
+(setq evil-cleverparens-complete-parens-in-yanked-region t)
+(setq evil-cleverparens-move-skip-delimiters t)
+(setq evil-cleverparens-swap-move-by-word-and-symbol t)
+(setq evil-cleverparens-drag-ignore-lines t)
+(setq evil-cleverparens-use-s-and-S nil)
+
 (require 'eval-in-repl-geiser)
 
 
