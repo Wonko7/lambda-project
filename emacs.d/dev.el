cc2eb3d06abd4a95defea57a97c194ff6035223d
William <wonko7@gmail.com>
2023-10-08 19:02:00 +0200

    [guix][shell] add local root to all guix shells

--- emacs.d/dev.el
+++ emacs.d/dev.el
@@ -142,7 +142,10 @@
 (setq buffer-env-script-name "guix.scm")
 (add-hook 'hack-local-variables-hook #'buffer-env-update)
 
-(add-hook 'eshell-directory-change-hook #'buffer-env-update) ;; overkill: revisit. but works.
+(setq buffer-env-commands '((".env" . "set -a && >&2 . \"$0\" && env -0")
+                            ("manifest.scm" . "guix shell -m \"$0\" -r .guix-root -- env -0")
+                            ("guix.scm" . "guix shell -D -f \"$0\" -r .guix-root -- env -0")
+                            ("*" . ">&2 . \"$0\" && env -0")))
 (add-hook 'utop-mode-hook #'hack-dir-local-variables-non-file-buffer) ;; this one doesn't
 (add-hook 'comint-mode-hook #'hack-dir-local-variables-non-file-buffer)
 
