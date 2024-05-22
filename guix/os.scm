26855a725f1ec6b83e95dbb0d4209a12080093d7
William <wonko7@gmail.com>
2024-05-22 17:43:42 +0200

    [guix][os] only start xfce on rocinante

--- guix/os.scm
+++ guix/os.scm
@@ -72,28 +72,34 @@
 ;; services
 
 (define (ship->services ship) ;; also depends on %fleet and %wonko
-  (let* ((fleet-desktop-base (list
-                              (service bluetooth-service-type
-                                       (bluetooth-configuration (auto-enable? #t)))
-                              (service
-                               slim-service-type
-                               (slim-configuration
-                                (display ":9")
-                                (vt "vt9")
-                                (auto-login? (not (string= (ship-name ship) "discovery")))
-                                (default-user (crew-name %wonko))
-                                (xorg-configuration (xorg-configuration
-                                                     (keyboard-layout (crew-kb %wonko))))))
-                              ;; (service
-                              ;;  slim-service-type
-                              ;;  (slim-configuration
-                              ;;   (display ":10")
-                              ;;   (vt "vt10")
-                              ;;   (auto-login? #t) ;; FIXME
-                              ;;   (default-user (crew-name %tina))
-                              ;;   (xorg-configuration (xorg-configuration
-                              ;;                        (keyboard-layout (crew-kb %tina))))))
-                              ))
+  (let* ((fleet-desktop-base
+          (filter service?
+                  (list
+                   (service bluetooth-service-type
+                            (bluetooth-configuration (auto-enable? #t)))
+
+                   (service
+                    slim-service-type
+                    (slim-configuration
+                     (display ":9")
+                     (vt "vt9")
+                     (auto-login?
+                      (and (not (string= (ship-name ship) "discovery"))
+                           (not (string= (ship-name ship) "rocinante"))))
+                     (default-user (crew-name %wonko))
+                     (xorg-configuration (xorg-configuration
+                                          (keyboard-layout (crew-kb %wonko))))))
+
+                   (when (string= (ship-name ship) "rocinante")
+                     (service
+                      slim-service-type
+                      (slim-configuration
+                       (display ":10")
+                       (vt "vt10")
+                       (auto-login? #t)
+                       (default-user (crew-name %tina))
+                       (xorg-configuration (xorg-configuration
+                                            (keyboard-layout (crew-kb %tina))))))))))
 
          (fleet-permanent-base (list (service guix-publish-service-type
                                               (guix-publish-configuration
