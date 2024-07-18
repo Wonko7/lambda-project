278d4952c8aa792dd3719eb16c135021f0b9f888
William <wonko7@gmail.com>
2024-07-18 18:45:22 +0200

    [guix][os] add rocinante to fleet deploy

--- wonko/systems/rocinante.scm
+++ wonko/systems/rocinante.scm
@@ -1,30 +1,33 @@
-(use-modules (gnu)
-             (gnu services shepherd)
-             (gnu services desktop)
-             (gnu services xorg)
-             (gnu services sddm)
-             (gnu services networking)
-             (gnu services ssh)
-             (gnu services guix)
-             (gnu home)
-             (gnu home services)
-             (gnu home services shepherd)
-             (gnu home services shells)
-             (guix build utils)
-             (guix gexp)
-             (ice-9 format)
-             (ice-9 match)
-             (srfi srfi-1)
-             (srfi srfi-11)
-             (srfi srfi-88)
-             ;; my stuff
-             (wonko defs)
-             (wonko crew)
-             (wonko fleet)
-             (wonko dotfiles)
-             (wonko xorg)
-             (wonko homes)
-             (wonko systems))
+(define-module (wonko systems rocinante)
+  #:use-module (gnu)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services xorg)
+  #:use-module (gnu services sddm)
+  #:use-module (gnu services networking)
+  #:use-module (gnu services ssh)
+  #:use-module (gnu services guix)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shepherd)
+  #:use-module (gnu home services shells)
+  #:use-module (guix build utils)
+  #:use-module (guix gexp)
+  #:use-module (ice-9 format)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-11)
+  #:use-module (srfi srfi-88)
+  ;; my stuff
+  #:use-module (wonko defs)
+  #:use-module (wonko crew)
+  #:use-module (wonko fleet)
+  #:use-module (wonko dotfiles)
+  #:use-module (wonko xorg)
+  #:use-module (wonko homes)
+  #:use-module (wonko systems)
+  #:export (%rocinante-wonko-home
+            %rocinante-os))
 
 (use-package-modules xorg)
 
@@ -49,41 +52,42 @@
               (xinput . "set-prop 'SynPS/2 Synaptics TouchPad' 'Tapping Drag Lock Enabled' 1")))))))
      %media-station-wonko-services))))
 
-(operating-system
-  (inherit %media-station-os)
-  (host-name "rocinante")
-  (services (cons* (service slim-service-type
-                            (slim-configuration
-                             (display ":10")
-                             (vt "vt10")
-                             (auto-login? #t)
-                             (default-user (crew-name %tina))
-                             (xorg-configuration (xorg-configuration
-                                                  (keyboard-layout (crew-kb %tina))))))
-                   (service noautostart-slim-service-type wonko-slim-config)
-                   (service guix-home-service-type
-                            `(("wonko" ,%rocinante-wonko-home)
-                              ("tina" ,%tina-home)))
-                   %media-station-services))
-  (mapped-devices
-   (list (mapped-device
-          (source (uuid "ec7a9b12-4611-469c-8a6f-aadf4d525d5e"))
-          (target "vault")
-          (type luks-device-mapping))))
+(define %rocinante-os
+  (operating-system
+    (inherit %media-station-os)
+    (host-name "rocinante")
+    (services (cons* (service slim-service-type
+                              (slim-configuration
+                               (display ":10")
+                               (vt "vt10")
+                               (auto-login? #t)
+                               (default-user (crew-name %tina))
+                               (xorg-configuration (xorg-configuration
+                                                    (keyboard-layout (crew-kb %tina))))))
+                     (service noautostart-slim-service-type wonko-slim-config)
+                     (service guix-home-service-type
+                              `(("wonko" ,%rocinante-wonko-home)
+                                ("tina" ,%tina-home)))
+                     %media-station-services))
+    (mapped-devices
+     (list (mapped-device
+            (source (uuid "ec7a9b12-4611-469c-8a6f-aadf4d525d5e"))
+            (target "vault")
+            (type luks-device-mapping))))
 
-  (file-systems (let ((btrfs-vault-subvol (lambda (args)
-                                            (make-vault-subvolume args mapped-devices))))
-                  (cons*
-                   (file-system
-                     (mount-point "/boot")
-                     (device (uuid "918C-B182"
-                                   'fat32))
-                     (type "vfat"))
-                   (file-system
-                     (mount-point "/mnt/vault")
-                     (device "/dev/mapper/vault")
-                     (type "btrfs")
-                     (dependencies mapped-devices))
-                   (append
-                    (make-vault-subvolumes mapped-devices)
-                    %base-file-systems)))))
+    (file-systems (let ((btrfs-vault-subvol (lambda (args)
+                                              (make-vault-subvolume args mapped-devices))))
+                    (cons*
+                     (file-system
+                       (mount-point "/boot")
+                       (device (uuid "918C-B182"
+                                     'fat32))
+                       (type "vfat"))
+                     (file-system
+                       (mount-point "/mnt/vault")
+                       (device "/dev/mapper/vault")
+                       (type "btrfs")
+                       (dependencies mapped-devices))
+                     (append
+                      (make-vault-subvolumes mapped-devices)
+                      %base-file-systems))))))
