eb30c8925079f71834e4b221e0f78fbf0887c87d
William <wonko7@gmail.com>
2024-12-17 23:42:06 +0100

    [guix] pin channels

--- wonko/systems.scm
+++ wonko/systems.scm
@@ -33,24 +33,60 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; channels
 
-(define %channels (cons*
-                   (channel
-                    (name 'nonguix)
-                    (url "https://gitlab.com/nonguix/nonguix")
-                    (introduction
-                     (make-channel-introduction
-                      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-                      (openpgp-fingerprint
-                       "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))
-                   (channel
-                    (name 'w7)
-                    (url "https://gitlab.com/wonko7/w7-guix-channel")
-                    (introduction
-                     (make-channel-introduction
-                      "e6afee0a2c3e941e186b2a9035c0217e5e94e9d5"
-                      (openpgp-fingerprint
-                       "FF23 0627 4DFE CF36 3AD8  677C 613C 8B66 6DBE 0AEB"))))
-                   %default-channels))
+(define %channels ;; this will pull latest
+  (cons*
+   (channel
+    (name 'nonguix)
+    (url "https://gitlab.com/nonguix/nonguix")
+    (introduction
+     (make-channel-introduction
+      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+      (openpgp-fingerprint
+       "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))
+   (channel
+    (name 'w7)
+    (url "https://gitlab.com/wonko7/w7-guix-channel")
+    (introduction
+     (make-channel-introduction
+      "e6afee0a2c3e941e186b2a9035c0217e5e94e9d5"
+      (openpgp-fingerprint
+       "FF23 0627 4DFE CF36 3AD8  677C 613C 8B66 6DBE 0AEB"))))
+   %default-channels))
+
+(define %channels ;; this is pinned, generated with `guix describe -f channels`
+  (list (channel
+         (name 'nonguix)
+         (url "https://gitlab.com/nonguix/nonguix")
+         (branch "master")
+         (commit
+          "10e3c2bcaedaba121eec0e255d366a080082cf0a")
+         (introduction
+          (make-channel-introduction
+           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+           (openpgp-fingerprint
+            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+        (channel
+         (name 'w7)
+         (url "https://gitlab.com/wonko7/w7-guix-channel")
+         (branch "master")
+         (commit
+          "069aba5ac5b306d29435f1882a10dce825404dc1")
+         (introduction
+          (make-channel-introduction
+           "e6afee0a2c3e941e186b2a9035c0217e5e94e9d5"
+           (openpgp-fingerprint
+            "FF23 0627 4DFE CF36 3AD8  677C 613C 8B66 6DBE 0AEB"))))
+        (channel
+         (name 'guix)
+         (url "https://git.savannah.gnu.org/git/guix.git")
+         (branch "master")
+         (commit
+          "942942ee75542e684baaccdd26372cfa6e2bc2a2")
+         (introduction
+          (make-channel-introduction
+           "9edb3f66fd807b096b48283debdcddccfea34bad"
+           (openpgp-fingerprint
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; vault subvolumes
