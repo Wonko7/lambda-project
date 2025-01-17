c250eb473c4d2696ca96b32e5cfc59ee00305c0a
William <wonko7@gmail.com>
2025-01-17 17:50:06 +0100

    [emacs][layouts][wip] eval stuff on the fly, add tramp-4

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -94,28 +94,96 @@
 
 ;; (require 'ace-link)
 
-(use-package window-layout
-  )
+(use-package window-layout)
 
 (defvar ws/current-layout (-repeat exwm-workspace-number nil))
 
-;; (plist-get '(:name code :buffer "init.el") ':name)
-
 (setq ws/layouts
-      `(( :layout code3
-          :recipe (-
-                   (:upper-size-ratio 0.8)
+      `(( :layout code2
+          :recipe (| (:left-size-ratio 0.5)
+                     left
+                     right)
+          :buffers ((:name left  :buffer-f (magit-status))
+                    (:name right :buffer-f (projectile-run-shell))))
+        ( :layout code2-bottom-shell
+          :recipe (- (:upper-size-ratio 0.8)
+                     (| (:left-size-ratio 0.5)
+                        left
+                        right)
+                     shell)
+          :buffers ((:name right :buffer-f (buffer-name))
+                    (:name left  :buffer-f (magit-status))
+                    (:name shell :buffer-f (projectile-run-shell))))
+        ( :layout code3
+          :recipe (| (:left-size-ratio 0.3)
+                     right
+                     (| (:left-size-ratio 0.5)
+                        center
+                        left))
+          :buffers ((:name center :buffer-f (buffer-name))
+                    (:name right  :buffer-f (magit-status))
+                    (:name left   :buffer-f (projectile-run-shell))))
+        ( :layout tramp-4
+          :recipe (| (:left-size-ratio 0.5)
+                     (- (:upper-size-ratio 0.7)
+                        topl
+                        botl)
+                     (- (:upper-size-ratio 0.7)
+                        topr
+                        botr))
+          :buffers-f (let* ((pr (projectile-acquire-root))
+                            (rm (consult--read
+                                 (remove "" (string-split (shell-command-to-string "cat /etc/hosts | cut -d\\\t -f2 | grep -v localhost") "\n"))
+                                 :prompt "choose ship from fleet: "
+                                 :sort nil
+                                 :require-match t))
+                            (rpr (concat "/ssh:" rm ":" pr)))
+                       `((:name topl :buffer-f (magit-status ,pr))
+                         (:name topr :buffer-f (magit-status ,rpr))
+                         ( :name botl
+                           :buffer-f (projectile-with-default-dir ,pr
+                                       (shell
+                                        (projectile-generate-process-name "shell" nil ,pr))))
+                         ( :name botr
+                           :buffer-f (projectile-with-default-dir ,rpr
+                                       (shell
+                                        (projectile-generate-process-name
+                                         "tramp-shell" nil ,rpr)))))))
+        ( :layout grid9
+          :recipe (|
+                   (:left-size-ratio 0.3)
+                   (-
+                    (:upper-size-ratio 0.3)
+                    a
+                    (-
+                     (:upper-size-ratio 0.5)
+                     b
+                     c))
                    (| (:left-size-ratio 0.5)
-                      code
-                      magit)
-                   shell)
-          :buffers ((:name code  :buffer "init.el")
-                    (:name magit ;;:buffer-f (filter-project-buffs "magit")
-                           :buffer "magit")
-                    (:name shell ;; :buffer-f (filter-project-buffs "*shell")
-                           :buffer "*shell")))))
+                      (-
+                       (:upper-size-ratio 0.3)
+                       d
+                       (-
+                        (:upper-size-ratio 0.5)
+                        e
+                        f))
+                      (-
+                       (:upper-size-ratio 0.3)
+                       g
+                       (-
+                        (:upper-size-ratio 0.5)
+                        h
+                        i))))
+          :buffers-f (mapcar
+                      (lambda (ab)
+                        (let ((a (car ab))
+                              (b (cdr ab)))
+                          `(:name ,a :buffer ,b)))
+                      (-zip '(a b c d e f g h i)
+                            (-filter (lambda (b)
+                                       (not (string-prefix-p " " (buffer-name b))))
+                                     (buffer-list)))))))
 
-;; (defun)
 (defun ws/set-layout ()
   (interactive)
   (let* ((layouts     (mapcar (lambda (lo)
@@ -128,11 +196,24 @@
                        :require-match t))
          (layout       (first (-filter (lambda (lo)
                                          (string= layout-name (plist-get lo ':layout)))
-                                       ws/layouts))))
+                                       ws/layouts)))
+         (buffs       (plist-get layout ':buffers-f))
+         (layout      (if buffs
+                          (plist-put layout ':buffers (eval buffs))
+                        layout)))
     (setf (nth exwm-workspace-current-index ws/current-layout)
-          (list layout
-                (wlf:layout (plist-get layout ':recipe)
-                            (plist-get layout ':buffers))))))
+          (list
+           layout
+           (wlf:layout
+            (plist-get layout ':recipe)
+            (mapcar
+             #'(lambda (b)
+                 (let* ((name (plist-get b ':name))
+                        (bf   (plist-get b ':buffer-f)))
+                   (if bf
+                       `(:buffer ,(eval bf) :name ,name)
+                     b)))
+             (plist-get layout ':buffers)))))))
 
 (defun ws/toggle-buffer ()
   (interactive)
@@ -149,87 +230,10 @@
                   :require-match t)))
     (wlf:toggle wm (intern bn))))
 
-(setq wm
-      (wlf:layout
-       '(-
-         (:upper-size-ratio 0.8)
-         (| (:left-size-ratio 0.5)
-            code
-            magit)
-         shell)
-       `((:name code :buffer "init.el")
-         (:name magit
-                :buffer ,(filter-project-buffs "magit"))
-         (:name shell
-                :buffer ,(filter-project-buffs "*shell")))))
-
-(wlf:toggle wm 'shell)
-(wlf:select wm 'shell)
-
-(setq wm
-      (wlf:layout
-       '(|
-         (:left-size-ratio 0.3)
-         shell
-         (| (:left-size-ratio 0.5)
-            code
-            magit))
-       `((:name code :buffer "init.el")
-         (:name magit
-                :buffer ,(filter-project-buffs "magit"))
-         (:name shell
-                :buffer ,(filter-project-buffs "*shell")))))
-
-(setq wm
-      (wlf:layout
-       '(|
-         (:left-size-ratio 0.3)
-         (-
-          (:upper-size-ratio 0.3)
-          c
-          (-
-           (:upper-size-ratio 0.5)
-           a
-           b))
-         (| (:left-size-ratio 0.5)
-            (-
-             (:upper-size-ratio 0.3)
-             f
-             (-
-              (:upper-size-ratio 0.5)
-              d
-              e))
-            (-
-             (:upper-size-ratio 0.3)
-             i
-             (-
-              (:upper-size-ratio 0.5)
-              g
-              h))))
-       (mapcar
-        (lambda (ab)
-          (let ((a (car ab))
-                (b (cdr ab)))
-            `(:name ,a :buffer ,b)))
-        (-zip '(a b c d e f g h i) (projectile-project-buffers)))))
-
-(mapcar
- (lambda (ab)
-   (let ((a (car ab))
-         (b (cdr ab)))
-     `(:name ,a :buffer ,b)))
- (-zip '(a b c d e f g h i) (projectile-project-buffers)))
-
-(mapcar
- #'buffer-name
- (projectile-project-buffers))
-
-(defun filter-project-buffs (name)
+(defun unused/filter-project-buffs (name)
   (first (-filter (lambda (b)
                     (string-prefix-p name (buffer-name b)))
                   (projectile-project-buffers))))
-
-(filter-project-buffs "magit")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; auth/pass
