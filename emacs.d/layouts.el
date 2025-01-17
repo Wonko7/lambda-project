9adb685b9d8c384728067a8369d0f0c548d5392e
William <wonko7@gmail.com>
2025-01-17 18:45:37 +0100

    [emacs][layouts] give more explicit names to buffers

--- emacs.d/layouts.el
+++ emacs.d/layouts.el
@@ -12,6 +12,7 @@
                        right)
             :buffers ((:name left  :buffer-f (magit-status))
                       (:name right :buffer-f (projectile-run-shell))))
+
           ( :layout code2-bottom-shell
             :recipe (- (:upper-size-ratio 0.8)
                        (| (:left-size-ratio 0.5)
@@ -21,6 +22,7 @@
             :buffers ((:name right :buffer-f (buffer-name))
                       (:name left  :buffer-f (magit-status))
                       (:name shell :buffer-f (projectile-run-shell))))
+
           ( :layout code3
             :recipe (| (:left-size-ratio 0.3)
                        right
@@ -30,28 +32,30 @@
             :buffers ((:name center :buffer-f (buffer-name))
                       (:name right  :buffer-f (magit-status))
                       (:name left   :buffer-f (projectile-run-shell))))
-          ( :layout tramp-4
+
+          ( :layout tramp4
             :recipe (| (:left-size-ratio 0.5)
                        (- (:upper-size-ratio 0.7)
-                          topl
-                          botl)
+                          local-code
+                          local-shell)
                        (- (:upper-size-ratio 0.7)
-                          topr
-                          botr))
-            :buffers-f (let* ((pr (projectile-acquire-root))
-                              (rm (my/choose-remote-from-fleet))
+                          remote-code
+                          remote-shell))
+            :buffers-f (let* ((pr  (projectile-acquire-root))
+                              (rm  (my/choose-remote-from-fleet))
                               (rpr (concat "/ssh:" rm ":" pr)))
-                         `((:name topl :buffer-f (magit-status ,pr))
-                           (:name topr :buffer-f (magit-status ,rpr))
-                           ( :name botl
+                         `((:name local-code  :buffer-f (magit-status ,pr))
+                           (:name remote-code :buffer-f (magit-status ,rpr))
+                           ( :name local-shell
                              :buffer-f (projectile-with-default-dir ,pr
                                          (shell
                                           (projectile-generate-process-name "shell" nil ,pr))))
-                           ( :name botr
+                           ( :name remote-shell
                              :buffer-f (projectile-with-default-dir ,rpr
                                          (shell
                                           (projectile-generate-process-name
                                            "tramp-shell" nil ,rpr)))))))
+
           ( :layout grid9
             :recipe (|
                      (:left-size-ratio 0.3)
@@ -84,6 +88,7 @@
                             `(:name ,a :buffer ,b)))
                         (-zip '(a b c d e f g h i)
                               (-filter (lambda (b)
+                                         ;; remove mini-buffers from list
                                          (not (string-prefix-p " " (buffer-name b))))
                                        (buffer-list)))))))
 
