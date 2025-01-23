ab116c4a000a79f567441d063cbdf48a773a6a89
William <wonko7@gmail.com>
2025-01-23 17:16:03 +0100

    [emacs][layouts] add optional argument to ws/set-layout

--- emacs.d/layouts.el
+++ emacs.d/layouts.el
@@ -113,18 +113,22 @@
                       ( :name right
                         :buffer-f (org-roam-node-open
                                    (org-roam-node-from-title-or-alias "📺 sense8")))))))
+
+  (defun ws/set-layout (&optional layout)
     (interactive)
     (let* ((layouts     (mapcar (lambda (lo)
                                   (plist-get lo ':layout))
                                 ws/layouts))
-           (layout-name (consult--read
-                         (mapcar #'symbol-name layouts)
-                         :prompt "layout? "
-                         :sort nil
-                         :require-match t))
-           (layout       (first (-filter (lambda (lo)
-                                           (string= layout-name (plist-get lo ':layout)))
-                                         ws/layouts)))
+           (layout-name (if (null layout)
+                            (consult--read
+                             (mapcar #'symbol-name layouts)
+                             :prompt "layout? "
+                             :sort nil
+                             :require-match t)
+                          (symbol-name layout)))
+           (layout      (first (-filter (lambda (lo)
+                                          (string= layout-name (plist-get lo ':layout)))
+                                        ws/layouts)))
            (buffs       (plist-get layout ':buffers-f))
            (layout      (if buffs
                             (plist-put layout ':buffers (eval buffs))
@@ -135,12 +139,12 @@
              (wlf:layout
               (plist-get layout ':recipe)
               (mapcar
-               #'(lambda (b)
-                   (let* ((name (plist-get b ':name))
-                          (bf   (plist-get b ':buffer-f)))
-                     (if bf
-                         `(:buffer ,(eval bf) :name ,name)
-                       b)))
+               (lambda (b)
+                 (let* ((name (plist-get b ':name))
+                        (bf   (plist-get b ':buffer-f)))
+                   (if bf
+                       `(:buffer ,(eval bf) :name ,name)
+                     b)))
                (plist-get layout ':buffers)))))))
 
   (defun ws/toggle-buffer ()
