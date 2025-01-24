8d70e6b2b4ef1d598a3b9c4958e4380757b7fa34
William <wonko7@gmail.com>
2025-01-24 17:41:56 +0100

    [guix][channels] remove obsolete w7 channel

--- wonko/systems.scm
+++ wonko/systems.scm
@@ -51,14 +51,6 @@
       "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
       (openpgp-fingerprint
        "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))
-   (channel
-    (name 'w7)
-    (url "https://gitlab.com/wonko7/w7-guix-channel")
-    (introduction
-     (make-channel-introduction
-      "e6afee0a2c3e941e186b2a9035c0217e5e94e9d5"
-      (openpgp-fingerprint
-       "FF23 0627 4DFE CF36 3AD8  677C 613C 8B66 6DBE 0AEB"))))
    %default-channels))
 
 (define %channels ;; this is pinned, generated with `guix describe -f channels`
@@ -73,17 +65,6 @@
            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
            (openpgp-fingerprint
             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-        (channel
-         (name 'w7)
-         (url "https://gitlab.com/wonko7/w7-guix-channel")
-         (branch "master")
-         (commit
-          "069aba5ac5b306d29435f1882a10dce825404dc1")
-         (introduction
-          (make-channel-introduction
-           "e6afee0a2c3e941e186b2a9035c0217e5e94e9d5"
-           (openpgp-fingerprint
-            "FF23 0627 4DFE CF36 3AD8  677C 613C 8B66 6DBE 0AEB"))))
         (channel
          (name 'guix)
          (url "https://git.savannah.gnu.org/git/guix.git")
