04a9f9b0b3e55cd005a3b4900bb21b5516b5e3c0
William <wonko7@gmail.com>
2025-01-25 11:44:37 +0100

    [emacs][shell][keys] add gS to toggle scroll to bottom

--- emacs.d/keys.el
+++ emacs.d/keys.el
@@ -269,6 +269,7 @@
   "("           #'comint-previous-prompt
   ")"           #'comint-next-prompt
   "gm"          #'man-follow
+  "gS"          #'my/toggle-scroll-to-bottom-on-output
   "C-k"         #'comint-previous-prompt
   "C-j"         #'comint-next-prompt
   "C-r"         #'consult-history
