bcf305633f07610a3b8037126b6dab3312e5f83c
William <wonko7@gmail.com>
2025-01-30 23:40:49 +0100

    [emacs][exwm] tune-alpha sets alpha-background

--- emacs.d/exwm.el
+++ emacs.d/exwm.el
@@ -85,12 +85,12 @@
 ;; misc functions, should move this?
 
 (defun my/tune-alpha (direction)
-  (let* ((a (frame-parameter (selected-frame) 'alpha))
-         (a (if a (car a) 100))
-         (a (+ a (if (string= direction "up") 2 -2)))
-         (a (if (> a 100) 100 a))
+  (let* ((a (frame-parameter (selected-frame) 'alpha-background))
+         (a (or a 1.0))
+         (a (+ a (if (string= direction "up") 0.05 -0.05)))
+         (a (if (> a 1.0) 1.0 a))
          (a (if (< a 0) 0 a)))
-    (set-frame-parameter (selected-frame) 'alpha (cons a 70))))
+    (set-frame-parameter (selected-frame) 'alpha-background a)))
 
 (defun my/tune-workspace (dir)
   (let* ((c exwm-workspace-current-index)
