22a77636199aae7236a4e9b1d6e22b418d027706
William <wonko7@gmail.com>
2025-02-18 19:56:26 +0100

    [emacs][keys] comment diff keys for now

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -198,15 +198,17 @@
     "K"    #'git-rebase-move-line-up
     "J"    #'git-rebase-move-line-down)
 
-  (general-evil-define-key '(normal) diff-hl-mode-map
-    "grk" #'smerge-prev
-    "grj" #'smerge-next
-    "C-k" #'smerge-prev
-    "C-j" #'smerge-next
-    "("   #'smerge-prev
-    ")"   #'smerge-next
-    "Ku"  #'smerge-keep-upper
-    "Kl"  #'smerge-keep-lower))
+  ;; => whaaat, overrides org :/
+  ;; (general-evil-define-key '(normal) diff-hl-mode-map
+  ;;   "grk" #'smerge-prev
+  ;;   "grj" #'smerge-next
+  ;;   "C-k" #'smerge-prev
+  ;;   "C-j" #'smerge-next
+  ;;   "("   #'smerge-prev
+  ;;   ")"   #'smerge-next
+  ;;   "Ku"  #'smerge-keep-upper
+  ;;   "Kl"  #'smerge-keep-lower)
+  )
 
 (use-package magit-todos
   :after magit
