5c5845fb95bb7f1af4efbaa7fd899655df8c597d
William <wonko7@gmail.com>
2025-04-14 20:28:35 +0200

    [guix][x][discovery] add setxkb to xsession

--- wonko/systems/discovery.scm
+++ wonko/systems/discovery.scm
@@ -48,7 +48,8 @@
                #$xinput "/bin/xinput"
                " set-prop 'ETPS/2 Elantech Touchpad' 'Synaptics Two-Finger Scrolling' 1 1;"
                #$xinput "/bin/xinput"
-               " set-prop 'ETPS/2 Elantech Touchpad' 'libinput Accel Speed' 0.7")))))))))
+               " set-prop 'ETPS/2 Elantech Touchpad' 'libinput Accel Speed' 0.7;"
+               #$setxkbmap "/bin/setxkbmap -option compose:ralt us;")))))))))
 
 (define %wonko-home
   (home-environment
