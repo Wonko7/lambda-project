0541188fd23615377947ff0f9646709620ded784
William <wonko7@gmail.com>
2025-07-01 21:14:51 +0200

    [guix] update guix mirror url

--- misc/guix-config/channels.scm
+++ misc/guix-config/channels.scm
@@ -20,7 +20,7 @@
  (list
   (channel
    (name 'guix)
-   (url "https://codeberg.org/guix/guix-mirror")
+   (url "https://codeberg.org/guix/guix")
    (branch "master")
    (introduction
     (make-channel-introduction
