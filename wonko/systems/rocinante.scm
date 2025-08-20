19847a3fbb0daf9b3f0fe47d976b17d6b1621139
William <wonko7@gmail.com>
2025-08-20 19:16:05 +0200

    [guix] add xfce4-desktop-service to rocinante

--- wonko/systems/rocinante.scm
+++ wonko/systems/rocinante.scm
@@ -29,6 +29,7 @@
   #:export (%rocinante-os))
 
 (use-package-modules xorg)
+(use-service-modules desktop)
 
 (define machine-home-services
   (list
@@ -71,12 +72,13 @@
     (locale "fr_FR.utf8")
     (services (cons* (service slim-service-type
                               (slim-configuration
-                               (display ":10")
-                               (vt "vt10")
-                               (auto-login? #t)
-                               (default-user (crew-name %tina))
-                               (xorg-configuration (xorg-configuration
-                                                    (keyboard-layout %fr-kb))))) ;; FIXME
+                                (display ":10")
+                                (vt "vt10")
+                                (auto-login? #t)
+                                (default-user (crew-name %tina))
+                                (xorg-configuration (xorg-configuration
+                                                      (keyboard-layout %fr-kb))))) ;; FIXME
+                     (service xfce-desktop-service-type)
                      (service noautostart-slim-service-type wonko-slim-config)
                      (service noautostart-slim-service-type media-station-slim-config)
                      (service guix-home-service-type
@@ -89,9 +91,9 @@
                      %laptop-services))
     (mapped-devices
      (list (mapped-device
-            (source (uuid "ec7a9b12-4611-469c-8a6f-aadf4d525d5e"))
-            (target "vault")
-            (type luks-device-mapping))))
+             (source (uuid "ec7a9b12-4611-469c-8a6f-aadf4d525d5e"))
+             (target "vault")
+             (type luks-device-mapping))))
     (file-systems (let ((btrfs-vault-subvol (lambda (args)
                                               (make-vault-subvolume args mapped-devices))))
                     (cons*
