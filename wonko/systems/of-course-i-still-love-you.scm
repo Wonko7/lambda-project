ec33230ad81c8aa083b2e632bcfa4190206e3be1
William <wonko7@gmail.com>
2025-08-22 10:57:16 +0200

    [guix] of-course-i-still-love-you: add zfs-load-key

--- wonko/systems/of-course-i-still-love-you.scm
+++ wonko/systems/of-course-i-still-love-you.scm
@@ -95,13 +95,22 @@
         (start #~(lambda _
                    (invoke/quiet #$zpool "import" "-a" "-N")))
         (stop #~(const #f))))
-    ;; (define zfs-load-key
-    ;;   (shepherd-service ...))
+    (define zfs-load-key
+      (shepherd-service
+        (provision '(zfs-load-key))
+        (documentation "Scans for ZFS pools.")
+        (requirement '(zfs-scan))
+        (modules scheme-modules)
+        (start #~(lambda _
+                   (invoke/quiet #$zfs "load-key"
+                                 "-L" "/root/.one-ring-to-rule-them-all" "babel")))
+        (stop #~(lambda _
+                  (invoke/quiet #$zfs "unload-key" "-r" "babel")))))
     (define zfs-automount
       (shepherd-service
         (provision '(zfs-automount))
         (documentation "Automounts ZFS data sets.")
-        (requirement '(zfs-scan))
+        (requirement '(zfs-scan zfs-load-key))
         (modules scheme-modules)
         (start #~(lambda _
                    (with-output-to-port (current-error-port)
@@ -112,8 +121,7 @@
                   (invoke/quiet #$zfs "unmount" "-a" "-f") #f))))
     ;; (define zfs-zed
     ;;   (shepherd-service ...))
-    ;; (list zfs-scan zfs-load-key zfs-automount zfs-zed)
-    (list zfs-scan zfs-automount)))
+    (list zfs-scan zfs-load-key zfs-automount)))
 
 (define %of-course-i-still-love-you-os
   (operating-system
