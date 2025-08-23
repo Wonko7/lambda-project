028d1c704c9ce335e76748a689cd2fe9a73db3e4
William <wonko7@gmail.com>
2025-08-23 15:06:29 +0200

    [emacs][fancy] add current workspace to modeline

--- emacs.d/fancy.el
+++ emacs.d/fancy.el
@@ -72,6 +72,47 @@
 ;; (setq doom-modeline-persp-icon nil)
 ;; (doom-modeline-mode)
 
+;; just remove minor modes:
+(setq mode-line-modes
+      (let ((recursive-edit-help-echo
+             "Recursive edit, type C-M-c to get out"))
+        (list (propertize "%[" 'help-echo recursive-edit-help-echo)
+	      "("
+	      `(:propertize ("" mode-name)
+			    help-echo "Major mode\n\
+mouse-1: Display major mode menu\n\
+mouse-2: Show help for major mode\n\
+mouse-3: Toggle minor modes"
+			    mouse-face mode-line-highlight
+			    local-map ,mode-line-major-mode-keymap)
+	      '("" mode-line-process)
+              ;; 	      `(:propertize ("" minor-mode-alist)
+              ;; 			    mouse-face mode-line-highlight
+              ;; 			    help-echo "Minor mode\n\
+              ;; mouse-1: Display minor mode menu\n\
+              ;; mouse-2: Show help for minor mode\n\
+              ;; mouse-3: Toggle minor modes"
+              ;; 			    local-map ,mode-line-minor-mode-keymap)
+	      (propertize "%n" 'help-echo "mouse-2: Remove narrowing from buffer"
+		          'mouse-face 'mode-line-highlight
+		          'local-map (make-mode-line-mouse-map
+				      'mouse-2 #'mode-line-widen))
+	      ")"
+	      (propertize "%]" 'help-echo recursive-edit-help-echo)
+	      " ")))
+
+;; add workspace:
+(defcustom exwm-mode-line-format
+  `((:propertize " " display (space :align-to (- right 6)))
+    (:propertize (:eval (format "🖥️%d" exwm-workspace-current-index))
+		 ;; local-map ,exwm-mode-line-workspace-map
+		 mouse-face mode-line-highlight
+                 ))
+  "EXWM workspace in the mode line."
+  :type 'sexp)
+
+(add-to-list 'mode-line-misc-info exwm-mode-line-format t)
+
 (setq mode-line-with-margin
       `(
         ;;'display '(space :width 1)
@@ -83,9 +124,6 @@
                         ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote)
                         ;; display (min-width (5.0))
                         )
-                       ;;"<" (exwm-get-ws-str) ">"
-                       ;;"<" (number-to-string exwm-workspace-current-index) ">"
-                       ;; (concat "<" (number-to-string exwm-workspace-current-index) ">")
                        mode-line-frame-identification
                        mode-line-buffer-identification
                        "   " mode-line-position evil-mode-line-tag "  " mode-line-modes mode-line-misc-info
@@ -102,7 +140,6 @@
             (propertize " " 'face 'fringe 'display `((space :width 1)))
             )))))
 
-(setq-default mode-line-format mode-line-with-margin)
 (setq mode-line-format mode-line-with-margin)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
