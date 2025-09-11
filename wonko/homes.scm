5768e55353a9712763f687a308e615464d7ef6ab
William <wonko7@gmail.com>
2025-09-11 17:48:52 +0200

    [guix] remove extraneous channels from borked pin

--- wonko/homes.scm
+++ wonko/homes.scm
@@ -105,28 +105,6 @@
   ;; ((name, packages, pinned channel))
   `(("borked" ,%borked-2025-08
      (list (channel
-            (name 'guix-forge)
-            (url "https://git.systemreboot.net/guix-forge/")
-            (branch "main")
-            (commit
-             "15b559c2f2e497ed059197f91937798411b8e365")
-            (introduction
-             (make-channel-introduction
-              "0432e37b20dd678a02efee21adf0b9525a670310"
-              (openpgp-fingerprint
-               "7F73 0343 F2F0 9F3C 77BF  79D3 2E25 EE8B 6180 2BB3"))))
-           (channel
-            (name 'nonguix)
-            (url "https://gitlab.com/nonguix/nonguix")
-            (branch "master")
-            (commit
-             "c16a92e3bef67a602bc56b0dd20ecdc8cac8c97f")
-            (introduction
-             (make-channel-introduction
-              "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-              (openpgp-fingerprint
-               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-           (channel
             (name 'guix)
             (url "https://codeberg.org/guix/guix-mirror")
             (branch "master")
