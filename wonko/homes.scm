8965fa35cb72cbd00069243d1c9596402ce192f1
William <wonko7@gmail.com>
2025-12-20 15:02:31 +0100

    [guix][shell][alias] dmesg: disable pager

--- wonko/homes.scm
+++ wonko/homes.scm
@@ -108,7 +108,7 @@
     ("g"     . "git")
     ("psrg"  . "ps aux | rg -M0")
     ("df"    . "df -h")
-    ("dmesg" . "dmesg -He")
+    ("dmesg" . "dmesg -PHe")
     ("free"  . "free -h")
     ("grep"  . "grep --color=auto")
     ("ls"    . "ls --color=auto")
