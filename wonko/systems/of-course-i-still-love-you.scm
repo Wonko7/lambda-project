56cec31687b1ff4118147b25b0eb829267656131
William <wonko7@gmail.com>
2026-01-02 16:58:56 +0100

    [guix][os][sly] nftables, radvd & static networking config

--- wonko/systems/of-course-i-still-love-you.scm
+++ wonko/systems/of-course-i-still-love-you.scm
@@ -16,6 +16,7 @@
   #:use-module (guix utils)
   #:use-module (guix packages)
   #:use-module (guix gexp)
+  #:use-module (guix records)
   #:use-module (ice-9 format)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
@@ -32,8 +33,12 @@
   #:export (%of-course-i-still-love-you-wonko-home
             %of-course-i-still-love-you-os))
 
-(use-package-modules file-systems xorg machine-learning synergy)
-(use-service-modules linux nfs)
+(use-package-modules file-systems xorg machine-learning synergy
+                     ;; public net:
+                     admin linux)
+(use-service-modules linux nfs
+                     ;; public net:
+                     configuration sysctl)
 
 (define machine-home-services
   (list
@@ -49,25 +54,25 @@
 
 (define %of-course-i-still-love-you-wonko-home
   (home-environment
-   (inherit %vanilla-wonko-home)
-   (services
-    (cons*
-     (simple-service
-      'of-course-i-still-love-you-shepherd home-shepherd-service-type
-      (list
-       (shepherd-service
-        (provision '(synergyc))
-        (auto-start? #f)
-        (start #~(make-forkexec-constructor
-                  (list #$(file-append synergy "/bin/synergyc")
-                        "-n" "media-station"
-                        "-f" "yggdrasill.local")
-                  #:log-file #$(home-log-path "synergy")))
-        (stop #~(make-kill-destructor))
-        (documentation "can't be arsed to move IRL"))))
-     (append
-      machine-home-services
-      %vanilla-wonko-services)))))
+    (inherit %vanilla-wonko-home)
+    (services
+     (cons*
+      (simple-service
+       'of-course-i-still-love-you-shepherd home-shepherd-service-type
+       (list
+        (shepherd-service
+          (provision '(synergyc))
+          (auto-start? #f)
+          (start #~(make-forkexec-constructor
+                    (list #$(file-append synergy "/bin/synergyc")
+                          "-n" "media-station"
+                          "-f" "yggdrasill.local")
+                    #:log-file #$(home-log-path "synergy")))
+          (stop #~(make-kill-destructor))
+          (documentation "can't be arsed to move IRL"))))
+      (append
+       machine-home-services
+       %vanilla-wonko-services)))))
 
 (define %media-station-home
   (home-environment
@@ -140,6 +145,114 @@
                      "-m" "/code/llms/phi-4-q4.gguf")))
      (stop #~(make-kill-destructor)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; public facing services network config:
+
+(define %nftables-ruleset
+  (plain-file "nftables.conf" "\
+table inet firewall {
+
+    chain inbound_ipv4 {
+        # icmp type echo-request limit rate 5/second accept
+        accept # accept everything on local network
+    }
+
+    chain inbound_ipv6 {
+        # accept neighbour discovery otherwise connectivity breaks
+        #
+        icmpv6 type { nd-neighbor-solicit, nd-router-advert, nd-neighbor-advert } accept
+
+        # accepting ping (icmpv6-echo-request) for diagnostic purposes.
+        # However, it also lets probes discover this host is alive.
+        # This sample accepts them within a certain rate limit:
+        #
+        icmpv6 type echo-request limit rate 5/second accept
+        ip6 daddr != 2a01:e0a:b5a:de71::/64 accept # accept on non public
+        tcp dport { 80, 443 } accept # only accept these on public facing ipv6
+    }
+
+    chain inbound {
+
+        # By default, drop all traffic unless it meets a filter
+        # criteria specified by the rules that follow below.
+        type filter hook input priority 0; policy drop;
+
+        # Allow traffic from established and related packets, drop invalid
+        ct state vmap { established : accept, related : accept, invalid : drop }
+
+        # Allow loopback traffic.
+        iifname lo accept
+        # drop connections to lo not coming from lo
+        iif != lo ip daddr 127.0.0.1/8 drop
+        iif != lo ip6 daddr ::1/128 drop
+
+        # Jump to chain according to layer 3 protocol using a verdict map
+        meta protocol vmap { ip : jump inbound_ipv4, ip6 : jump inbound_ipv6 }
+
+        # Allow SSH on port TCP/22 and allow HTTP(S) TCP/80 and TCP/443
+        # for IPv4 and IPv6.
+        # tcp dport { 22, 80, 443} accept
+
+        # Uncomment to enable logging of denied inbound traffic
+        # log prefix \"[nftables] Inbound Denied: \" counter drop
+    }
+
+    chain forward {
+        # Drop everything (assumes this device is not a router)
+        type filter hook forward priority 0; policy drop;
+    }
+
+    # no need to define output chain, default policy is accept if undefined.
+} "))
+
+(define %radvd-config
+  (plain-file "radvd.conf" "\
+interface eth0                    # identifies the interface we are advertising on
+{                                 # if you need, you can have multiple interfaces defined
+
+    AdvSendAdvert on;             # this simply means that we are sending advertisements
+
+    MinRtrAdvInterval 5;          # these options control how often advertisements are sent
+    MaxRtrAdvInterval 15;         # these aren't mandatory, but valuable settings
+                                  # I will discuss them in a moment
+
+    prefix 2a01:e0a:b5a:de71::/64 # netmask length must be '/64' (see RFC 2462, sect 5.5.3, page 18)
+    {
+        AdvOnLink on;             # Says to a host: 'Everyone sharing this prefix is on the same,' 'local link as you.'
+        AdvAutonomous on;         # Says to a host: 'Use this prefix to autoconfigure your address.'
+    };
+};"))
+
+(define-configuration/no-serialization radvd-configuration
+  (config-file
+   (file-like %radvd-config)
+   "A file-like object containing radvd config."))
+
+(define (radvd-shepherd-service config)
+  (match-record config <radvd-configuration>
+                (config-file)
+    (shepherd-service
+      (documentation "Linux IPv6 Router Advertisement Daemon (radvd)")
+      (provision '(radvd))
+      (start #~(make-forkexec-constructor
+                `(#$(file-append radvd "/sbin/radvd")
+                  "-n" "-C" #$config-file)))
+      (stop #~(make-kill-destructor)))))
+
+
+(define radvd-service-type
+  (service-type
+    (name 'radvd)
+    (description "radvd")
+    (extensions
+     (list (service-extension shepherd-root-service-type
+                              (compose list radvd-shepherd-service))
+           ;; (service-extension profile-service-type
+           ;;                    (compose list radvd-configuration))
+           ))
+    (default-value (radvd-configuration))))
+
+
 (define %of-course-i-still-love-you-os
   (operating-system
 
@@ -171,6 +284,7 @@
                                        "  Option \"AsyncFlipSecondaries\" \"false\"\n"
                                        "EndSection\n")))))
        (cons*
+        ;; zfs
         (simple-service 'zfs-loader kernel-module-loader-service-type '("zfs"))
         (simple-service 'zfs-shepherd-services
                         shepherd-root-service-type
@@ -178,6 +292,8 @@
         (simple-service 'zfs-sheperd-services-user-processes
                         user-processes-service-type
                         '(zfs-automount))
+
+        ;; my users & desktop usage:
         (service slim-service-type (slim-configuration
                                      (inherit wonko-slim-config)
                                      (xorg-configuration xorg-cfg)))
@@ -193,7 +309,35 @@
         (simple-service 'skynet-llm-service
                         shepherd-root-service-type
                         skynet-llm-service)
-        %media-station-services)))
+
+        ;; <!-- public net stuff:
+        (service nftables-service-type (nftables-configuration
+                                         (ruleset %nftables-ruleset)))
+        (service radvd-service-type (radvd-configuration
+                                     (config-file %radvd)))
+        (modify-services %media-station-services
+          (sysctl-service-type
+           config =>
+           (sysctl-configuration
+             (settings (append '(("net.ipv6.conf.all.forwarding" . "1"))
+                               %default-sysctl-settings))))
+          ;; okkkkk... nm did not like this, will probably end nuking nm.
+          ;; (static-networking-service-type
+          ;;  config =>
+          ;;  (list
+          ;;   (static-networking
+          ;;     (addresses
+          ;;      (cons (network-address
+          ;;              (device "eth0")
+          ;;              (value "2a01:e0a:b5a:de71::1/64"))
+          ;;            (static-networking-addresses %loopback-static-networking)))
+          ;;     (routes
+          ;;      (list (network-route
+          ;;              (destination "2000::/3")
+          ;;              (gateway "2a01:e0a:b5a:de70::1"))))
+          ;;     (provision '(loopback)))))
+          ;; public net stuff -->
+          ))))
 
     (mapped-devices
      (list (mapped-device
