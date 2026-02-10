978f96574c1bd566a90c885f6861512b2c9c06aa
William <wonko7@gmail.com>
2026-02-10 22:34:22 +0100

    [emacs][wip] maxipass.at's sql

--- emacs.d/maxipassat.el
+++ emacs.d/maxipassat.el
@@ -1,110 +1,51 @@
 ;;; maxipassat.el  -*- lexical-binding: t; -*-
 
-(org-ql-sparse-tree '(org-ql-query
-
-                       :select nil
-                       :from   '("/data/org/here-be-dragons/20210825151927-mont_ussy.org"
-                                 "/data/org/here-be-dragons/the-road-so-far/2024-05-28.org")
-                       :where '(and (tags "pub")
-                                    (tags "mp"))))
-
-(let ((maxi-passat (org-ql-query
-                     :select nil
-                     :from   '("/data/org/here-be-dragons/20210825151927-mont_ussy.org"
-                               "/data/org/here-be-dragons/the-road-so-far/2024-05-28.org")
-                     :where '(and (tags "pub")
-                                  (tags "mp")
-                                  (not (tags "is" "crypt"))))))
-  (object-print
-
-   maxi-passat))
-(org-ql-select (org-agenda-files)
-  '(todo "WAITING")
-  :action 'element)
-(let ((maxi-passat (org-ql-select
-                     '("/data/org/here-be-dragons/20210825151927-mont_ussy.org"
-                       "/data/org/here-be-dragons/the-road-so-far/2024-05-28.org")
-                     '(and (tags "pub")
-                           (tags "mp")
-                           (not (tags "is" "crypt")))
-:action 'element
-                     )))
-  (object-print maxi-passat))
 
 
-(require 'org-element)
-
-(setq-local org-use-tag-inheritance nil)
-
-(tt-print-heading-has-id
- (get-file-buffer "/data/org/here-be-dragons/the-road-so-far/2024-05-28.org"))
-
-
-(tt-print-heading-has-id ())
-(defun tt-print-heading-has-id (b)
-  "print headings that have property named :ID: and print the property value.
-2019-01-14"
-  (interactive)
-  (with-current-buffer b
-    (with-output-to-temp-buffer "what"
-      (org-element-map (org-element-parse-buffer) 'headline
-        (lambda (x)
-          (let ((myid (org-element-property :ID x)))
-            (when t
-              (princ "heading: " )
-              (princ (org-element-property :raw-value x))
-              ;;(terpri )
-              ;; (princ "ID value is: " )
-              ;; (princ myid )
-              (princ (org-element-interpret-data x))
-              (terpri )
-              (princ "children: " )
-              (princ (org-element-contents x) )
-              (mapc (lambda (x)
-                      (princ "child: " )
-                      (princ x)
-                      (terpri))
-                    (org-element-map (org-element-contents x) 'org-element-all-elements
-                      #'org-element-interpret-data ))
-              (terpri )
-              )))))))
-
-
-(defun tt-print-heading-has-id (b)
-  "print headings that have property named :ID: and print the property value.
-2019-01-14"
-  (interactive)
-  (display (org-element-parse-buffer)))
-
-;; dev
-(add-to-list 'load-path "/code/emacs/org-ml")
-(add-to-list 'load-path "/code/emacs/org-sql")
-(require 'org-ml)
-(require 'org-sql)
+(defun org-sql--disk-get-hashpathpairs ()
+  "Get a list of hashpathpair for org files on disk.
+Each hashpathpair will have it's :db-path set to nil. Only files in
+`org-sql-files' will be considered."
+  (cl-flet
+      ((get-md5
+         (fp)
+         (org-sql--on-success (org-sql--run-command "md5sum" `(,fp) nil)
+                              (car (s-split-up-to " " it-out 1))
+                              (error "Could not get md5")))
+       (expand-if-path
+         (fp)
+         (list fp)))
+    (if (stringp org-sql-files)
+        (error "`org-sql-files' must be a list of paths")
+      (--map (cons (get-md5 it) it) org-sql-files))))
 
 (setq org-sql-db-config '(postgres
-                         :hostname "localhost"
-                         :port 3000
-                         :username "wonko"
-                         :schema "org"
-                         :database "maxi_passat"))
+                          :hostname "localhost"
+                          :port 3000
+                          :username "wonko"
+                          :schema "org"
+                          :database "maxipassat"))
 
-(org-sql-user-init)
+;; (org-sql-user-init)
 
-(defun my/tmp-files ()
-  (org-ql-search-directories-files
-   :recurse t
-   :directories (list org-roam-directory)
-   ))
-
-(setq org-sql-async nil)
-(setq org-sql-debug t)
 (setq org-sql-files
-      '("/data/org/here-be-dragons/20210825151927-mont_ussy.org"
-        "/data/org/here-be-dragons/the-road-so-far/_archive/2024-05-28.org"
-        "/data/org/here-be-dragons/test.org"
-        "/data/org/here-be-dragons/20211031184333-gorges_du_houx.org"
+      '(
+        ;; "here-be-dragons/wtf/20230822110052-hiking.org"
+        "here-be-dragons/.www/maxipassat/en/greeting.org"
+        "here-be-dragons/.www/maxipassat/fr/greeting.org"
+        "here-be-dragons/.www/maxipassat/testing-123.org"
+        "here-be-dragons/tech/20230412204446-shell.org"
+        ;; "/data/org/here-be-dragons/tech/20201102214323-ocaml.org"
+        "here-be-dragons/tech/20210621102226-postgresql.org"
+        "here-be-dragons/_the-road-so-far/-archive/2025-10-01.org"
+        "here-be-dragons/wtf/20210905155320-wtf.org"
+        "here-be-dragons/media/20260208124347-twin_peaks.org"
         ))
 
-(setq org-sql-files (my/tmp-files ))
+(setq sql-postgres-program "/gnu/store/lgjbah69qnv6px06mkg8hgyfbksa6wrp-profile/bin/psql" )
+(setq org-sql--psql-exe "/gnu/store/lgjbah69qnv6px06mkg8hgyfbksa6wrp-profile/bin/psql" )
+
+(setq default-directory "/data/org/")
+
 (org-sql-user-push)
+(org-sql-clear-db)
