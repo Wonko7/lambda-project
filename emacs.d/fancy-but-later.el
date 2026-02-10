f8c17ee2404e8ad585d8382986fb1fa21c14da12
William <wonko7@gmail.com>
2026-02-10 11:22:06 +0100

    [emacs][fold] move folding to fancy-but-later

--- emacs.d/fancy-but-later.el
+++ emacs.d/fancy-but-later.el
@@ -45,4 +45,68 @@
   :config
   (nyan-mode 1))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; folding
+
+(use-package outline-indent
+  :commands (outline-indent--update-ellipsis outline-indent-minor-mode)
+  :hook
+  (makefile-mode-hook . outline-indent-minor-mode)
+  :custom
+  (outline-indent-ellipsis " ▼")
+  :config
+  (defun outline-toggle-children ()
+    (interactive)
+    (save-excursion
+      (outline-back-to-heading)
+      (if (not (outline-invisible-p (pos-eol)))
+          (outline-hide-subtree)
+        (outline-show-subtree) ;; <- this is the diff
+        (outline-show-entry)))))
+
+(use-package hideshow
+  :after outline-indent
+  :hook
+  (emacs-lisp-mode-hook . hs-minor-mode)
+  (emacs-lisp-mode-hook . outline-indent--update-ellipsis)
+  (scheme-mode-hook . hs-minor-mode)
+  (scheme-mode-hook . outline-indent--update-ellipsis))
+
+(use-package seq) ;; TODO: dash/seq/cl-lib => use cl everywhere?
+(use-package origami
+  :after (evil seq outline-indent)
+  :custom
+  (origami-fold-replacement " ▼")
+  :hook
+  (tuareg-mode-hook . origami-mode)
+  (org-agenda-mode-hook . origami-mode)
+  :config
+  (let* ((op origami-parser-alist))
+    (setq origami-parser-alist (append origami-parser-alist
+                                       `((tuareg-mode . origami-indent-parser)))))
+  (defun origami-recursively-toggle-node (buffer point)
+    ;; diff is rm `if last-command` that won't work wrapped in evil-fold.
+    (interactive (list (current-buffer) (point)))
+    (-when-let (path (origami-search-forward-for-path buffer point))
+      (let ((node (-last-item path)))
+        (cond ((origami-fold-node-recursively-open? node)
+               (origami-close-node-recursively buffer (origami-fold-beg node)))
+              ((origami-fold-node-recursively-closed? node)
+               (origami-toggle-node buffer (origami-fold-beg node)))
+              (t (origami-open-node-recursively buffer (origami-fold-beg node)))))))
+  (setq evil-fold-list
+        (cons
+         `((origami-mode)
+           :open-all   ,(lambda () (origami-open-all-nodes (current-buffer)))
+           :close-all  ,(lambda () (origami-close-all-nodes (current-buffer)))
+           ;; setting this for toggle:
+           :toggle     ,(lambda () (origami-recursively-toggle-node (current-buffer) (point)))
+           :open       ,(lambda () (origami-open-node (current-buffer) (point)))
+           :open-rec   ,(lambda () (origami-open-node-recursively (current-buffer) (point)))
+           :close      ,(lambda () (origami-close-node (current-buffer) (point))))
+         (seq-filter (lambda (actions)
+                       (let ((m (caar actions)))
+                         (not (equal m 'origami-mode))))
+                     evil-fold-list))))
+
 (provide 'conf/fancy-but-later)
