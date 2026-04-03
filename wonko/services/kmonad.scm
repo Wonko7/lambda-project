95ea88b7908ab8c8b56eb09d63b1691319bf311b
William <wonko7@gmail.com>
2026-04-03 14:55:47 +0200

    [kbd] commit to new whitespace modifier position

--- wonko/services/kmonad.scm
+++ wonko/services/kmonad.scm
@@ -262,7 +262,7 @@
   '(deflayer dance-commander
      esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  @LLL
      grv  1    2    3    4    5    6    7    8    9    0    @Csp @CP  bspc  ins  home pgup
-     tab  @Qs  @com @dot p    @yW  @fW  g    c    r    @ls  /    @C:  \     del  end  pgdn
+     tab  @Qs  @com @dot p    y    f    g    c    r    @ls  /    @C:  \     del  end  pgdn
      @EC  @ac  @oS  @em  @uW  i    d    @hW  @tm  @nS  @sc  -    @RC
      lsft @Smc q    j    k    x    b    m    w    v    @Sz  rsft                 up
      lalt @Tsy @mDB           spc            @mDB ralt @Tsy @Tsy            left down rght))
