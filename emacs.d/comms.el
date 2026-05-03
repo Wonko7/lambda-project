c5e71aae64194e53ac6264b0bdc3c6a86057c1eb
William <wonko7@gmail.com>
2026-05-03 15:23:18 +0200

    [emacs][comms] move some of erc config

--- emacs.d/comms.el
+++ emacs.d/comms.el
@@ -165,8 +165,8 @@
 
   (defun my/ement-init ()
     (interactive)
-    (ement-connect :user-id "@wonko7:matrix.org"
-                   :password (auth-source-pass-get 'secret "web/matrix/wonko7")
+    (ement-connect :user-id my/ement-id
+                   :password (auth-source-pass-get 'secret my/ement-pass-id)
                    :uri-prefix "http://127.0.0.1:8666"))
 
   (defun my/ement-home ()
@@ -179,16 +179,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; erc
 
-(defun my/irc-init ()
-  (interactive)
-  (require 'erc)
-  (setq erc-nick ""
-        erc-user-full-name ""
-        erc-dcc-verbose t
-        erc-modules '(page pcomplete netsplit fill button match track completion readonly networks ring autojoin noncommands irccontrols move-to-prompt stamp menu list))
-  (erc-update-modules)
-  (erc-tls :server "irc.irchighway.net"
-           :port   "6697"))
+(use-package erc
+  :custom
+  (erc-dcc-verbose t)
+  (erc-kill-buffer-on-part t)
+  (erc-auto-query 'bury))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; mastodon
