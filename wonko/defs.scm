5f8b259e63acd4ebf02a9f44e05aa043775bbd98
William <wonko7@gmail.com>
2026-06-23 15:45:01 +0200

    [guix][defs] add sys profile path

--- wonko/defs.scm
+++ wonko/defs.scm
@@ -5,16 +5,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; general defs:
 
-(define-public %lambda-project "/code/lambda-project")
-
-(define-public %guix-extra-profiles-dir "$HOME/.guix-extra-profiles")
 
 (define-public %font "JetBrains Mono Medium")
 (define-public %font-feh "JetBrainsMono-Regular")
 
-(define-public %wallpaper "/data/docs/pics/wallpapers/nasa-poster-vision-future/1 - 8XMgqaI.png")
 (define-public %lock-cmd
   '("/run/setuid-programs/xlock" "-mode" "daisy" "-lockdelay" "10"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; kbd
 
 (define-public %dvorak-kb
   (keyboard-layout "us" "dvorak" #:options '("ctrl:nocaps")))
@@ -23,18 +22,35 @@
 (define-public %us-kb
   (keyboard-layout "us" "qwerty" #:options '("ctrl:nocaps")))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; record
+
 (define-public (check predicate)
   (lambda (v)
     (if (predicate v)
         v
         (display (string-append "bad record value : " v)))))
 
-(define-public %home-log-root ".run/log/")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; sys paths
 
+(define-public %lambda-project "/code/lambda-project")
+(define-public %sys-profile-path "/run/current-system/profile/bin/")
+
+(define-public %wake-up-notification-file "/run/systemd/wakeup")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; dome paths
+
+(define-public %home-log-root ".run/log/")
 (define-public (home-log-path fn)
   #~(string-append (getenv "HOME") "/" #$%home-log-root #$fn ".log"))
 
-(define-public %wake-up-notification-file "/run/systemd/wakeup")
+(define-public %wallpaper "/data/docs/pics/wallpapers/nasa-poster-vision-future/1 - 8XMgqaI.png")
+(define-public %guix-extra-profiles-dir "$HOME/.guix-extra-profiles")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; colours
 
 (define-public %background-colour "#27212E")
 (define-public %foreground-hl-colour "#EB64B9")
