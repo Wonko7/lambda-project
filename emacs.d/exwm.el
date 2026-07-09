abd944cbef0f014e7b5eb4f75ecdf449246acf77
William <wonko7@gmail.com>
2026-07-09 12:21:08 +0200

    [emacs][evil][exwm][xim] try disabling evil in exwm mode

--- emacs.d/exwm.el
+++ emacs.d/exwm.el
@@ -277,6 +277,10 @@
 (use-package exwm-xim
   :demand t
   :config
+  (define-advice evil-disabled-buffer-p (:before-until () no-exwm)
+    (eq major-mode 'exwm-mode))
+  (evil-set-initial-state 'exwm-mode 'emacs)
+  (setq my/xim t)
   (exwm-xim-mode 1))
 
 (use-package exwm-randr
