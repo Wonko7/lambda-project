4673febeac8c5c489838d41495ab0bfa76535031
William <wonko7@gmail.com>
2026-09-03 10:49:23 +0200

    [guix][os] add divya-lambda channel

--- wonko/systems.scm
+++ wonko/systems.scm
@@ -51,20 +51,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; channels
 
-(define %channels ;; this will pull latest
-  (cons*
-   (channel
-    (name 'nonguix)
-    (url "https://gitlab.com/nonguix/nonguix")
-    (introduction
-     (make-channel-introduction
-      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-      (openpgp-fingerprint
-       "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))
-   %default-channels))
-
 (define %channels ;; this is pinned, generated with `guix describe -f channels`
   (list (channel
+          (name 'divya-lambda)
+          (url "https://codeberg.org/divyaranjan/divya-lambda.git")
+          (branch "master")
+          (commit "06337dde1619b9f3886f7ba3004b37fc02198fcf")
+          (introduction
+           (make-channel-introduction
+            "fe2010125fcbe003de42436b1a73ab53cc5e8288"
+            (openpgp-fingerprint
+             "F0B3 1A69 8006 8FB8 096A  2F12 B245 10C6 108C 8D4A"))))
+        (channel
           (name 'maxipassat)
           (url "https://codeberg.org/wonko/maxipassat")
           (branch "master")
