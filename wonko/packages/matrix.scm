e753c39739cfc50a77f0f2b0be177f7915cba7a4
William <wonko7@gmail.com>
2026-09-10 17:39:15 +0200

    [guix][pkg] vbump matrix's pantalaimon

--- wonko/packages/matrix.scm
+++ wonko/packages/matrix.scm
@@ -10,27 +10,28 @@
   #:use-module (wonko defs))
 
 (define-public img-pantalaimon
-  (let ((commit "e4a14b0a9b876abb1913b953457de15a9fc06d32")
+  (let ((commit "9fe0e801284b8afe11071443b3143b419fc95d27")
         (revision "0"))
     (package
       (inherit pantalaimon)
-      (name "pantalaimon")
-
-      (version (git-version "0.10.5" revision commit))
+      (name "img-pantalaimon")
+      (version (git-version "0.10.6" revision commit))
       (source
        (origin
          (method git-fetch)
          (uri (git-reference
-               (url "https://github.com/matrix-org/pantalaimon")
-               (commit commit)))
+                (url "https://github.com/matrix-org/pantalaimon") ;; archived??
+                (commit commit)))
          (file-name (git-file-name name version))
          (patches (list
                    (local-file
                     (string-append %lambda-project
                                    "/wonko/packages/patches/pantalaimon-img.patch"))))
          (sha256
-          (base32 "1qzlkl3z2y9crf12alaqp279flhd4g1hdpygp9bl81x4zxwjrmbp"))))
+          (base32 "07cf5k0b1i5pfplmx2p5l3ba76jjfzzcky9ylj7593k7nrm5drl3"))))
       (arguments
        (substitute-keyword-arguments (package-arguments pantalaimon)
          ((#:phases phases) #~(modify-phases #$phases
                                 (delete 'check))))))))
+
+img-pantalaimon
